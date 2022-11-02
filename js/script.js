8a079fccb1fcfaae077132a57b6f6d92ca2f427e
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-02 09:39:41 -0400

    change

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-// Copyright (c) 2022 Allen Li All rights reserved
-//
-// Created by: Allen Li
+// Created by: Allen li
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-"use strict"
-
+'use strict'
 /**
- * Check servie worker.
+ * This function calculates area and perimeter of triangle.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit3-02-HTML-lighthouse/sw.js", {
-    scope: "/ICS2O-Unit3-02-HTML-lighthouse/",
-  })
-}
-
-/**
- * This function displays an alert.
- */
-function calculateVolumeClicked() {
-  // input
-  const length = parseFloat(document.getElementById("length").value)
-  const width = parseFloat(document.getElementById("width").value)
-  const height = parseFloat(document.getElementById("height").value)
-
+function calculate () {
+  const radius = parseInt(document.getElementById('radius-of-circle').value)
   // process
-  const volume = (length * width * height) / 3
-
+  const volume = (4 / 3 * Math.PI * radius ** 3)
   // output
-  document.getElementById("volume").innerHTML =
-    "Volume is: " + volume.toFixed(2) + " mm³."
+    document.getElementById("volume").innerHTML = "<p>Volume is " + volume.toFixed(2) + " cm³</p>"
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/ICS2O-Unit3-02-HTML-lighthouse"
+var GHPATH = "/ICS2O-Unit3-03-Volume-of-Sphere"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
