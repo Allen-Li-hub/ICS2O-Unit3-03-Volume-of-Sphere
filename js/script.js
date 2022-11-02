e38912fadb1228a1d73bb7e817355d11c719796c
Allen-Li-hub <Allen-Li-hub@users.noreply.github.com>
2022-11-02 18:26:46 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates area and perimeter of triangle.
  */
-function calculate () {
-  const radius = parseInt(document.getElementById('radius-of-circle').value)
+function calculate() {
+  const radius = parseInt(document.getElementById("radius-of-circle").value)
   // process
-  const volume = (4 / 3 * Math.PI * radius ** 3)
+  const volume = (4 / 3) * Math.PI * radius ** 3
   // output
-    document.getElementById("volume").innerHTML = "<p>Volume is " + volume.toFixed(2) + " cm³</p>"
+  document.getElementById("volume").innerHTML =
+    "<p>Volume is " + volume.toFixed(2) + " cm³</p>"
 }
